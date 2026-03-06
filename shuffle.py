ed10072abd39970547ecf3ff57fa484409f6a2f5
kbh3rd <kbh3rd@kbh3rd.com>
2026-03-06 13:04:49 -0600

    comments and cleanup

--- shuffle.py
+++ shuffle.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
 """ Perform 'perfect' interleave shuffles of a standard deck of cards.
     What do the hands look like?
-    $Revision: 1.5 $    $Locker:  $
+    $Revision: 1.6 $    $Locker:  $
 """
 
 from terminalcolors import TerminalColors
 
 tc = TerminalColors()
 
-def suit_str(suit) :
-    if suit == "hearts":
-        return f"{tc.set_color('red')}♥"
-    elif suit == "diamonds":
-        return f"{tc.set_color('red')}♦"
-    elif suit == "clubs":
-        return f"{tc.set_color('black')}♣"
-    elif suit == "spades":
-        return f"{tc.set_color('black')}♠"
-    else :
-        return "?"
-
 class card :
+    """ One card. A deck is a list of these objects.
+    """
     use_color = True # use color coding in string representation
     by_face_value = 1 # compare by face value setting
     by_suit = 2  # compare by suit then face value setting
     compare_mode = by_face_value # 1=by value; 2=by suit then value
-    suit_value = {  # for sorting by suit then face value
+    suit_value = {  # for sorting by suit
         "♥": 100, "hearts": 100,
         "♣": 200, "clubs": 200,
         "♦": 300,  "diamonds":300,
         "♠": 400, "spades": 400
     }
-    face_value = { # for sorting by face value (not any gin game value)
+    face_value = { # for sorting by face value (not a card value in any gin game)
         "2": 2,
         "3": 3,
         "4": 4,
@@ -46,23 +36,28 @@ class card :
         "K": 13,
         "A": 14,
     }
+
     def __init__ (self, suit:str, face_value) :
-        #self.cardface = suit_str(suit) + str(value) + tc.reset()
-        if suit == "hearts":
+        """ create one card.
+            'suit' is name or symbol of one of the 4 standard suits
+            'face_value' is number or first letter of face card
+        """
+        if suit.lower() == "hearts" or suit == "♥" :
             self.color=tc.set_color('red')
             self.suit="♥"
-        elif suit == "diamonds":
+        elif suit.lower() == "diamonds" or suit == "♦" :
             self.color=tc.set_color('red')
             self.suit="♦"
-        elif suit == "clubs":
+        elif suit.lower() == "clubs" or suit == "♣" :
             self.color=tc.set_color('black')
             self.suit="♣"
-        elif suit == "spades":
+        elif suit.lower() == "spades" or suit == "♠" :
             self.color=tc.set_color('black')
             self.suit="♠"
-        self.face = str(face_value)
+        self.face = str(face_value).upper()
 
     def __str__(self) :
+        """ class variable determines if color is used or not """
         if card.use_color :
             return f"{self.color}{self.face}{self.suit}{tc.reset()}"
         else :
@@ -70,10 +65,12 @@ class card :
 
     @classmethod
     def set_nocolor(cls) :
+        """ Sets class variable for whether to use color or not in __str__()"""
         cls.use_color = False ;
 
     @classmethod
     def set_compareby(cls, setting) :
+        """ Sets class variable for card comparison methods used to sort a hand or deck """
         if setting == cls.by_face_value or setting == cls.by_suit :
             cls.compare_mode = setting
         else :
@@ -81,27 +78,34 @@ class card :
 
     @classmethod
     def compare_cards(cls, card1, operator, card2) :
+        """ Compare two cards with the given operator considering the class comparison mode.
+            If comparing by suit, add suite value (large) and face value (small) to effect
+            comparing by suit then face.
+        """
         val1 = cls.face_value[card1.face]
         val2 = cls.face_value[card2.face]
         if cls.compare_mode == cls.by_suit :
             val1 += cls.suit_value[card1.suit]
             val2 += cls.suit_value[card2.suit]
 
-        if operator == "gt" :
+        if operator == "gt" or operator == ">" :
             return val1 > val2
-        elif operator == "ge" :
+        elif operator == "ge" or operator == ">=" :
             return val1 >= val2
-        elif operator == "lt" :
+        elif operator == "lt" or operator == "<" :
             return val1 < val2
-        elif operator == "le" :
+        elif operator == "le" or operator == "<=" :
             return val1 <= val2
-        elif operator == "eq" :
+        elif operator == "eq" or operator == "==" :
             return val1 == val2
-        elif operator == "ne" :
+        elif operator == "ne" or operator == "!=" :
             return val1 != val2
         else :
             raise ValueError
 
+    # Methods used to compare two cards, either directly
+    # by the coder; "if card1 < card2:"
+    # or implicitly by sorted()
     def __gt__(self, other) :
         return card.compare_cards(self, "gt", other)
     def __ge__(self, other) :
@@ -116,6 +120,10 @@ class card :
         return card.compare_cards(self, "ne", other)
 
 def shuffle_halves(lst):
+    """ Splits a list in two halves and returns a new list
+        consisting of the halves regularly interleaved.
+        (AI assisted creation of this function, simple as it is.)
+    """
     half_len = len(lst) // 2
     first_half = lst[:half_len]
     second_half = lst[half_len:]
@@ -152,7 +160,7 @@ shuffles = int(args.shuffles)
 if args.bw :
     card.set_nocolor()
 
-# create the deck
+# create the deck -- a list of cards
 deck = list()
 for suit in SUITS :
     for face in range (2,11):
@@ -164,27 +172,27 @@ for suit in SUITS :
 for n in range(shuffles) :
     deck = shuffle_halves(deck)
 
-# print the shuffled deck
+# print the shuffled deck if being verbose
 if args.verbose :
     print (f"Size of deck: {len(deck)} cards")
-    print (f"After {shuffles} shuffles:")
+    print (f"After {shuffles} shuffle{'s' if shuffles > 1 else ''}:")
     for card in deck :
         print ("", card)
 
 
-# init deal of number of hands
+# initialize deal as a list of empty hands
 deal = []
 for hand in range(hands) :
     deal.append([])
 
-# fill the hands in the deal
+# fill the hands in the deal in the proper order
 topcard = 0
 for count in range (cardcount) :
     for hand in range(hands) :
         deal[hand].append(deck[topcard])
         topcard += 1
 
-# show the hands
+# show the hands dealt from the shuffled deck
 if args.suit :
     card.set_compareby(card.by_suit)
 else :
